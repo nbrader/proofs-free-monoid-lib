c697d299d644d6d9c99a2a31bfdacc02afbd1bf4
nbrader <nbrader+github@gmail.com>
2024-04-26 21:27:34 +0100

    Added another extra lemma

--- Coq/ExtraLemmas.v
+++ Coq/ExtraLemmas.v
@@ -4,3 +4,15 @@ Proof.
   intros A x y b.
   destruct b; reflexivity.
 Qed.
+
+Definition mul_1_l : forall n : nat, 1 * n = n :=
+  nat_ind
+    (fun n0 : nat => 1 * n0 = n0)
+    (eq_refl : 1 * 0 = 0)  (* Proof of the base case *)
+    (fun (n' : nat) (IHn' : 1 * n' = n') =>
+      eq_ind_r
+        (fun n0 : nat => S n0 = S n')
+        eq_refl
+        IHn'  (* Application of the induction hypothesis *)
+    : 1 * S n' = S n').
+    
