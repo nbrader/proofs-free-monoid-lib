17cdfe5c2ceb0939bf59798d126155000aad45b1
nbrader <nbrader+github@gmail.com>
2024-04-27 16:52:58 +0100

    Simplified use of theorem.

--- Coq/MonoidFreeTest.v
+++ Coq/MonoidFreeTest.v
@@ -23,20 +23,16 @@ Instance nat_Monoid : Monoid nat := {
 
 Axiom Basis_to_nat : MonoidFree.Basis -> nat.
 
-(* Define example_function using the Basis from MonoidFree directly *)
-Definition example_function (b : MonoidFree.Basis) : nat := Basis_to_nat b * 2.
-
 (* Define a proposition that asserts something about lifted_function *)
 Theorem lifted_function_correct : forall x y z : MonoidFree.Basis,
-  (MonoidFree.extend example_function) [x; y; z] = 2 * Basis_to_nat x + 2 * Basis_to_nat y + 2 * Basis_to_nat z.
+  (MonoidFree.extend (fun b => 2*Basis_to_nat b)) [x; y; z] = 2*Basis_to_nat x + 2*Basis_to_nat y + 2*Basis_to_nat z.
 Proof.
   intros x y z.
-  unfold MonoidFree.extend, example_function.
+  unfold MonoidFree.extend.
   simpl.
   rewrite !Nat.add_assoc.  (* Use associativity of addition to simplify the nested additions *)
   ring.
 Qed.
-
 
 Axiom b0 : MonoidFree.Basis.
 Axiom b1 : MonoidFree.Basis.
@@ -47,12 +43,9 @@ Axiom basis_b1 : Basis_to_nat b1 = 1.
 Axiom basis_b2 : Basis_to_nat b2 = 2.
 Axiom basis_b3 : Basis_to_nat b3 = 3.
 
-Theorem example_theorem : (MonoidFree.extend example_function) [b0; b1; b2] = 2*(0+1+2).
+Theorem example_theorem : (MonoidFree.extend (fun b => 2*Basis_to_nat b)) [b0; b1; b2] = 6.
 Proof.
   rewrite lifted_function_correct.
-  rewrite basis_b0.
-  rewrite basis_b1.
-  rewrite basis_b2.
-  simpl.
+  rewrite basis_b0, basis_b1, basis_b2.
   reflexivity.
 Qed.
