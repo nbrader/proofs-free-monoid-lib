116524b680216d9caedec9db0339f1c7aaae5284
nbrader <nbrader+github@gmail.com>
2024-04-27 13:59:44 +0100

    Changed Hom to Mor so as not to be confused with the Functor

--- Coq/Category.v
+++ Coq/Category.v
@@ -1,13 +1,13 @@
 Class Category := {
   Obj : Type;  (* Type of objects in the category *)
-  Hom : Obj -> Obj -> Type;  (* Type of morphisms between objects *)
+  Mor : Obj -> Obj -> Type;  (* Type of morphisms between objects *)
   
-  id : forall {X}, Hom X X;  (* Identity morphism *)
-  compose : forall {X Y Z}, Hom Y Z -> Hom X Y -> Hom X Z;  (* Composition of morphisms *)
+  id : forall {X}, Mor X X;  (* Identity morphism *)
+  compose : forall {X Y Z}, Mor Y Z -> Mor X Y -> Mor X Z;  (* Composition of morphisms *)
 
   (* Category axioms *)
-  left_identity : forall {X Y} (f : Hom X Y), compose id f = f;
-  right_identity : forall {X Y} (f : Hom X Y), compose f id = f;
-  associativity : forall {X Y Z W} (f : Hom W X) (g : Hom X Y) (h : Hom Y Z),
+  left_identity : forall {X Y} (f : Mor X Y), compose id f = f;
+  right_identity : forall {X Y} (f : Mor X Y), compose f id = f;
+  associativity : forall {X Y Z W} (f : Mor W X) (g : Mor X Y) (h : Mor Y Z),
                   compose h (compose g f) = compose (compose h g) f
 }.
--- Coq/TypeCategory.v
+++ Coq/TypeCategory.v
@@ -3,7 +3,7 @@ Require Import FreeMonoid.Category.
 (* Category of Coq types and functions *)
 Instance TypeCategory : Category := {
   Obj := Type;
-  Hom := fun X Y => X -> Y;
+  Mor := fun X Y => X -> Y;
   id := fun X x => x;
   compose := fun X Y Z g f x => g (f x);
   left_identity := fun X Y f => eq_refl;
