331613222780dba6f4134f593059624d1a8c842d
nbrader <nbrader+github@gmail.com>
2024-04-27 16:04:40 +0100

    An attempt at utilising generic MonoidFree file by using an Axiom which converts the generic "Basis" into a specific type.

---
+++ Coq/MonoidFreeTest.v
@@ -0,0 +1,46 @@
+Require Import FreeMonoid.MonoidFree.
+Require Import FreeMonoid.StructMagma.
+Require Import FreeMonoid.StructSemigroup.
+Require Import FreeMonoid.StructMonoid.
+Require Import Coq.Arith.Plus.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import ArithRing Ring.
+
+Instance nat_Magma : Magma nat := {
+  m_op := plus
+}.
+
+Instance nat_Semigroup : Semigroup nat := {
+  sg_assoc := Nat.add_assoc
+}.
+
+Instance nat_Monoid : Monoid nat := {
+  mn_id := 0;
+  mn_left_id := Nat.add_0_l;
+  mn_right_id := Nat.add_0_r
+}.
+
+Axiom Basis_to_nat : MonoidFree.Basis -> nat.
+
+(* Define example_function using the Basis from MonoidFree directly *)
+Definition example_function (b : MonoidFree.Basis) : nat := Basis_to_nat b * 2.
+
+(* Define a proposition that asserts something about lifted_function *)
+Theorem lifted_function_correct : forall x y z : MonoidFree.Basis,
+  (MonoidFree.extend example_function) [x; y; z] = 2 * Basis_to_nat x + 2 * Basis_to_nat y + 2 * Basis_to_nat z.
+Proof.
+  intros x y z.
+  unfold MonoidFree.extend, example_function.
+  simpl.
+
+  (* Expand the definition of fold_right assuming extend uses it for lists *)
+  unfold fold_right.
+
+  (* Simplify the computation of fold_right. It should fold the function over [x, y, z] starting with 0:
+     2 * Basis_to_nat x + (2 * Basis_to_nat y + (2 * Basis_to_nat z + 0)) *)
+  rewrite !Nat.add_assoc.  (* Use associativity of addition to simplify the nested additions *)
+
+  (* Now, you may manually unfold and simplify, or use a tactic like 'ring' to simplify arithmetic *)
+  ring.
+Qed.
