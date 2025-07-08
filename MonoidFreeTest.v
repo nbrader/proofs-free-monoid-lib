5d385bd7eb6ac7d0f9149b8f544e0ef8f18dc88d
nbrader <nbrader+github@gmail.com>
2025-07-08 20:36:04 +0100

    Made a more general comparison of mconcat and foldMap

--- MonoidFreeTest.v
+++ MonoidFreeTest.v
@@ -37,10 +37,15 @@ Proof.
   ring.
 Qed.
 
-Theorem example2 : forall (A : Type), forall (A_magma : Magma A), forall (A_semigroup : Semigroup A), forall (A_monoid : Monoid A), forall P : (nat -> A),
-  NatFreeMonoid.foldMap A_monoid P [3; 2; 1; 0] = mconcat A P 4.
+Theorem foldMap_mconcat_comparison_1 : forall (A : Type), forall (A_magma : Magma A), forall (A_semigroup : Semigroup A), forall (A_monoid : Monoid A), forall P : (nat -> A), forall n : nat,
+  NatFreeMonoid.foldMap A_monoid P (@mconcat _ _ _ NatFreeMonoid.FreeMonoid_Monoid (fun x => [x]) n) = mconcat A P n.
 Proof.
-  reflexivity.
+  intros.
+  induction n.
+  - reflexivity.
+  - simpl.
+    rewrite IHn.
+    reflexivity.
 Qed.
 
 (* Compute (NatFreeMonoid.foldMap nat_Monoid (fun b => 2*b) [0; 1; 2]). *)
